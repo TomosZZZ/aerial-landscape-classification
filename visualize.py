5222a567a8503a6f8b08545413e3e93d56b88910
TomosZZZ <okninski.t2004@gmail.com>
2026-03-24 09:43:18 +0000

    refactor: Make visualize plot with english labels

--- visualize.py
+++ visualize.py
@@ -6,7 +6,7 @@ def plot_training_results(history_path="history.json"):
         with open(history_path, "r") as f:
             history = json.load(f)
     except FileNotFoundError:
-        print(f"Błąd: Nie znaleziono pliku {history_path}. Czy trening się zakończył?")
+        print(f"Error file not found")
         return
 
     epochs = range(1, len(history['train_loss']) + 1)
@@ -14,19 +14,19 @@ def plot_training_results(history_path="history.json"):
     plt.figure(figsize=(14, 5))
 
     plt.subplot(1, 2, 1)
-    plt.plot(epochs, history['train_loss'], 'bo-', label='Trening')
-    plt.plot(epochs, history['val_loss'], 'ro-', label='Walidacja')
-    plt.title('Strata (Loss) w czasie')
-    plt.xlabel('Epoki')
+    plt.plot(epochs, history['train_loss'], 'bo-', label='Training')
+    plt.plot(epochs, history['val_loss'], 'ro-', label='Validation')
+    plt.title('Loss in time')
+    plt.xlabel('Epochs')
     plt.ylabel('Loss')
     plt.legend()
     plt.grid(True)
 
     plt.subplot(1, 2, 2)
-    plt.plot(epochs, history['train_acc'], 'bo-', label='Trening')
-    plt.plot(epochs, history['val_acc'], 'ro-', label='Walidacja')
-    plt.title('Dokładność (Accuracy) w czasie')
-    plt.xlabel('Epoki')
+    plt.plot(epochs, history['train_acc'], 'bo-', label='Training')
+    plt.plot(epochs, history['val_acc'], 'ro-', label='Validation')
+    plt.title('Accuracy in time')
+    plt.xlabel('Epochs')
     plt.ylabel('Accuracy')
     plt.legend()
     plt.grid(True)
